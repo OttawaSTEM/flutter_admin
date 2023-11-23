cea7e986fcccb0dc751187ceed2112eb101d6d5e
OttawaSTEM <ottawastem@gmail.com>
2023-11-23 11:20:48 -0500

    update sidemenu width

--- lib/constants/sizes.dart
+++ lib/constants/sizes.dart
@@ -1,8 +1,8 @@
 const double screenLarge = 1000;
 const double screenMedium = 500;
 
-const double sideMenuScreenFullWidth = 200;
-const double sideMenuScreenIconWidth = 80;
+const double sideMenuScreenFullWidth = 160;
+const double sideMenuScreenIconWidth = 50;
 const double sideMenuScreenZeroWidth = 0;
 const double sideMenuIconSize = 20;
 
